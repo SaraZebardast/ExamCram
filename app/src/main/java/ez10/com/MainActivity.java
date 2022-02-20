0522df02deaf06fffef9f9d930930c057e45b86d
Yassin Y <yasseenhaitham@gmail.com>
2022-02-20 15:17:18 +0300

    added simple back end for button

--- app/src/main/java/ez10/com/MainActivity.java
+++ app/src/main/java/ez10/com/MainActivity.java
@@ -2,13 +2,20 @@ package ez10.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+    }
+
+    public void onClick(View view) { //login button onclick lister
+        setContentView(R.layout.loading);
     }
 }
