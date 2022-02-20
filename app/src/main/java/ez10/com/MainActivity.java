7b996edf3957b73dfee674bd8b75f65037b20eea
Yassin Y <yasseenhaitham@gmail.com>
2022-02-20 16:33:29 +0300

    adjustments

--- app/src/main/java/ez10/com/MainActivity.java
+++ app/src/main/java/ez10/com/MainActivity.java
@@ -2,31 +2,40 @@ package ez10.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText usernameInput, passwordInput;
-
+    private EditText usernameInput, passwordInput;
+    private Button login;
+    private TextView incorrectInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        usernameInput = (EditText) findViewById(R.id.usernameIN);
-        passwordInput = (EditText) findViewById(R.id.passwordIN);
+        usernameInput = findViewById(R.id.usernameIN);
+        passwordInput = findViewById(R.id.passwordIN);
+        login = findViewById(R.id.loginButton);
+        incorrectInfo = findViewById(R.id.textView);
 
     }
 
     public void onLogin(View view) { //login button onclick lister
-        setContentView(R.layout.loading);
-        if (usernameInput.getText().toString()=="a") {
-            setContentView(R.layout.login);
+        String usernameInputStr = usernameInput.getText().toString();
+        String passwordInputStr = passwordInput.getText().toString();
+        if (usernameInputStr.equals("admin") && passwordInputStr.equals("admin") ) {
+            setContentView(R.layout.loading);
         }
+        else {
+            incorrectInfo.setText("Incorrect username or password");
+        }
+
+
 
     }
 }
