e6390aa6ce7c762f91366e2668e56e08a044837a
Yassin Y <yasseenhaitham@gmail.com>
2022-02-20 16:03:25 +0300

    loading animation

--- app/src/main/java/ez10/com/MainActivity.java
+++ app/src/main/java/ez10/com/MainActivity.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        usernameInput = (EditText) findViewById(R.id.usernameIN);
+        passwordInput = (EditText) findViewById(R.id.passwordIN);
+
     }
 
-    public void onClick(View view) { //login button onclick lister
+    public void onLogin(View view) { //login button onclick lister
         setContentView(R.layout.loading);
+        if (usernameInput.getText().toString()=="a") {
+            setContentView(R.layout.login);
+        }
+
     }
 }
