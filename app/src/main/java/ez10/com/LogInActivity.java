10d14868603c8c7f480b0c381c6887297e89cf0b
Yassin Y <yasseenhaitham@gmail.com>
2022-02-27 12:23:47 +0300

    HUGE UI UPDATE

--- app/src/main/java/ez10/com/LogInActivity.java
+++ app/src/main/java/ez10/com/LogInActivity.java
@@ -9,12 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 
 public class LogInActivity extends AppCompatActivity {
 
     private EditText usernameInput, passwordInput, createUserName;
     private Button login;
-    private TextView incorrectInfo ,createAccount;
+    private TextView incorrectInfo ,createAccount, welcomeBack, weMissedYou;
+    LottieAnimationView animationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +27,19 @@ public class LogInActivity extends AppCompatActivity {
         login = findViewById(R.id.loginButton);
         incorrectInfo = findViewById(R.id.textView);
         createAccount = findViewById(R.id.createAccount);
+        welcomeBack = findViewById(R.id.welcomeback);
+        weMissedYou = findViewById(R.id.wemissedyou);
+        animationView = findViewById(R.id.animationView);
     }
 
     public void onLogin(View view) { //login button onclick lister
         String usernameInputStr = usernameInput.getText().toString();
         String passwordInputStr = passwordInput.getText().toString();
         if (usernameInputStr.equals("admin") && passwordInputStr.equals("admin") ) {
-            setContentView(R.layout.loading);
+            weMissedYou.setVisibility(View.INVISIBLE);
+            welcomeBack.setVisibility(View.INVISIBLE);
+            animationView.setVisibility(View.VISIBLE);
+
         }
         else {
             incorrectInfo.setText("Incorrect username or password");
