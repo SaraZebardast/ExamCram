078496926f952b2caece21105833ffbf9c18592e
Yassin Y <yasseenhaitham@gmail.com>
2022-03-06 02:13:10 +0300

    added database dependencies, added user class, updated signup class to store data for out future database

--- app/src/main/java/ez10/com/SignUpActivity.java
+++ app/src/main/java/ez10/com/SignUpActivity.java
@@ -5,29 +5,70 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import android.os.Bundle;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
     Button button;
     TextView text;
     LottieAnimationView anim;
+    private FirebaseAuth mAuth;
+    EditText firstName, lastName, email, password;
+    CheckBox termsAndConditions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        mAuth = FirebaseAuth.getInstance();
         button= findViewById(R.id.createButton);
-        anim = findViewById(R.id.welcomeAnim);
+        anim = findViewById(R.id.loading);
+        firstName = findViewById(R.id.FirstName);
+        lastName = findViewById(R.id.LastName);
+        email = findViewById(R.id.EmailIN);
+        password = findViewById(R.id.passwordIN);
         text = findViewById(R.id.textinsignuppage);
     }
     public void onSignup(View view) {
-        button.setVisibility(View.INVISIBLE);
+        //button.setVisibility(View.INVISIBLE);
+        String emailStr = email.getText().toString().trim();
+        String passwordStr = password.getText().toString().trim();
+        String firstNameStr = firstName.getText().toString().trim();
+        String lastNameStr = lastName.getText().toString().trim();
+        if(firstNameStr.isEmpty()) {
+            firstName.setError("Your name cannot be empty");
+            firstName.requestFocus();
+            return;
+        }
+        if(lastNameStr.isEmpty()) {
+            lastName.setError("Your name cannot be empty");
+            lastName.requestFocus();
+            return;
+        }
+        if(emailStr.isEmpty()) {
+            email.setError("Please enter a valid email.");
+            email.requestFocus();
+            return;
+        }
+        if(passwordStr.isEmpty() || passwordStr.length()<8) {
+            password.setError("Your password must be at least 8 characters long.");
+            password.requestFocus();
+            return;
+        }
+        if (!termsAndConditions.isEnabled()) {
+            termsAndConditions.setError("You must agree to terms and conditions to continue.");
+            return;
+        }
+
         text.setVisibility(View.INVISIBLE);
         anim.setVisibility(View.VISIBLE);
 
---
+++ app/src/main/java/ez10/com/User.java
@@ -0,0 +1,15 @@
+package ez10.com;
+
+public class User {
+    String firstName, lastName, email, password;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }}
