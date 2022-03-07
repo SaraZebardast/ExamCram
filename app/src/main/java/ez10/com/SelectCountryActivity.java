f48b94ddd303cb255ec486d7751baf296a41f80e
Yassin Y <yasseenhaitham@gmail.com>
2022-03-07 19:15:07 +0300

    .

--- app/src/main/java/ez10/com/SelectCountryActivity.java
+++ app/src/main/java/ez10/com/SelectCountryActivity.java
@@ -6,13 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class SelectCountryActivity extends AppCompatActivity {
+
+    private Spinner countryChoice, uniChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_country);
+        countryChoice.findViewById(R.id.country);
+        uniChoice.findViewById(R.id.university);
+
+//        countryChoice.setOnItemClickListener(new AdapterView.OnItemSelectedListener() {
+//
+//            @Override
+//            public void onItemSelected(AdapterView<?> arg0, View arg1,
+//                                       int arg2, long arg3) {
+//
+//                String items=spinner.getSelectedItem().toString();
+//            }
+//
+//        });
     }
 
     public void onNext (View view) {
