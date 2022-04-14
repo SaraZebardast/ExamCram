fce4eb41ce9cefcc49b19e5a13fca7941ed1f31f
Pelin Öner <plnoner2003@gmail.com>
2022-04-14 15:59:17 +0300

    profile pic

--- app/src/main/java/ez10/com/profileselection.java
+++ app/src/main/java/ez10/com/profileselection.java
@@ -1,14 +1,37 @@
 package ez10.com;
 
+import com.google.android.material.button.MaterialButton;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class profileselection extends AppCompatActivity {
+public class profileselection extends AppCompatActivity implements View.onClickListener{
+    private ImageSwitcher imageSwitcher;
+    private MaterialButton nextButton;
+    private Integer ListImage[] = {R.drawable.steve, R.drawable.marya, R.drawable.einstein};
+    private Integer imageLength = ListImage.length;
+    private Integer counter = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profileselection);
+
+        imageSwitcher = findViewById(R.id.imageSwitcher);
+        nextButton = findViewById(R.id.nextButton);
+
+        public View makeView() {
+
+        });
+    }
+    protected void onClick(View view){
+
     }
 }
