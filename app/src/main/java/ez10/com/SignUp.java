7848a01d88d37f378a3c2efa7960b7e2b98042b6
Yassin Y <yasseenhaitham@gmail.com>
2022-05-01 15:22:51 +0300

    database changes v2

--- app/src/main/java/ez10/com/SignUp.java
+++ app/src/main/java/ez10/com/SignUp.java
@@ -18,8 +18,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUp extends AppCompatActivity {
 
@@ -126,6 +129,24 @@ public class SignUp extends AppCompatActivity {
                             for (int i=0; i<5; i++) {
                                 reference.child("friend" + i).setValue("-");
                             }
+
+                            reference = rootNode.getReference("Registered Universities/" + university + "/NoOfPeopleOnCampus");
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    String val = "" + snapshot.getValue();
+                                    DatabaseReference reference = rootNode.getReference("Registered Universities/" + university + "/NoOfPeopleOnCampus");
+
+                                    if (val.equals("null")) {
+                                        reference.setValue(0);
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
 
                             Intent intent = new Intent(SignUp.this, SelectProfilePicture.class);
 
