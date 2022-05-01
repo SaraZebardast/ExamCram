200c97e8025f1dd3faa44543cd6c6cbc012eb2da
Sara Zebardast <sara.zebardast@ug.bilkent.edu.tr>
2022-05-01 19:19:49 +0300

    Course select front end polished
    incomplete version!

---
+++ app/src/main/java/ez10/com/CourseSelect.java
@@ -0,0 +1,14 @@
+package ez10.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.course_select);
+    }
+}
